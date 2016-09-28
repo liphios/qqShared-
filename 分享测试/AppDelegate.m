85f0703ae69c28868af0fd1ab8e1f0be1c3109a3
李鹏辉 <liphios@163.com>
2016-09-28 14:49:30 +0800

    关于qq分享和qq空间分享的测试
    关于qq分享和qq空间分享的测试

--- 分享测试/AppDelegate.m
+++ 分享测试/AppDelegate.m
@@ -7,13 +7,30 @@
 //
 
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+@interface AppDelegate ()<QQApiInterfaceDelegate>
 
 @end
 
 @implementation AppDelegate
 
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
+    if ([url.scheme isEqualToString:[NSString stringWithFormat:@"tencent%@",@"1104987830"]]) {//QQ
+        return [TencentOAuth HandleOpenURL:url];
+    }else{
+        return YES;
+    }
+}
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
+    
+    if ([url.scheme isEqualToString:[NSString stringWithFormat:@"tencent%@",@"1104987830"]]) {//QQ
+        return [TencentOAuth HandleOpenURL:url];
+    }else{
+        return YES;
+    }
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -46,6 +63,47 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+
+//处理分享成功或者失败的信息
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+    // 因为在我们的应用中可能不止实现一种分享途径，可能还有微信啊 微博啊这些，所以在这里最好判断一下。
+    // QQAPPID:是你在QQ开放平台注册应用时候的AppID
+    if ([url.scheme isEqualToString:[NSString stringWithFormat:@"tencent%@",@"1104987830"]]) {
+        return [QQApiInterface handleOpenURL:url delegate:self];
+    }else {
+        return YES;
+    }
+}
+
+#pragma mark -- QQApiInterfaceDelegate
+// 处理来自QQ的请求，调用sendResp
+- (void)onReq:(QQBaseReq *)req {
+    
+}
+- (void)onResp:(QQBaseReq *)resp {
+    switch (resp.type)
+    {
+        case ESENDMESSAGETOQQRESPTYPE:
+        {
+            SendMessageToQQResp* sendResp = (SendMessageToQQResp*)resp;
+            if ([sendResp.result isEqualToString:@"0"]) {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"成功" message:@"QQ分享成功" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [alert show];
+            }
+            else {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"失败" message:@"QQ分享失败" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [alert show];
+            }
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
+
 
 
 @end
--- 分享测试/ViewController.m
+++ 分享测试/ViewController.m
@@ -7,22 +7,73 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+@interface ViewController ()<TencentSessionDelegate>
+@property (nonatomic, strong) TencentOAuth *tencentOAuth;
 @end
 
+#define Width [UIScreen mainScreen].bounds.size.width
+#define Height [UIScreen mainScreen].bounds.size.height
+#define RandomColor [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1.0]
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSArray *btnArr = @[@"分享到qq",@"分享到qq空间"];
+    for (int i = 0; i < 2; i++) {
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+        btn.frame = CGRectMake(Width / 2 - 50, (i+1)*100, 100, 30);
+        btn.tintColor = RandomColor;
+        [btn setTitle:btnArr[i] forState:UIControlStateNormal];
+        btn.tag = i;
+        [btn addTarget:self action:@selector(clickBtn:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+        
+    }
+}
+
+//点击了按钮的响应事件
+
+- (void)clickBtn:(UIButton *)btn{
+    
+    
+    switch (btn.tag) {
+        case 0://分享到QQ
+            [self showMediaNewsWithScene:0];
+            break;
+            
+        case 1://分享到QQ空间
+            [self showMediaNewsWithScene:1];
+            break;
+        default:
+            break;
+    }
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+//分享到qq和qq空间的消息
+- (void)showMediaNewsWithScene:(int)scene {
+    if (![TencentOAuth iphoneQQInstalled]) {
+        NSLog(@"请移步App Store去下载腾讯QQ客户端");
+    }else {
+        self.tencentOAuth = [[TencentOAuth alloc] initWithAppId:@"1104987830" andDelegate:self];
+        QQApiNewsObject *newsObj = [QQApiNewsObject
+                                    objectWithURL:[NSURL URLWithString:@"www.baidu.com"]
+                                    title:@"李易峰撞车了"
+                                    description:@"李易峰的兰博基尼被撞了李易峰的兰博基尼被撞了李易峰的兰博基尼被撞了"
+                                    previewImageURL:nil];
+        SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:newsObj];
+        if (scene == 0) {
+            NSLog(@"QQ好友列表分享 - %d",[QQApiInterface sendReq:req]);
+        }else if (scene == 1){
+            NSLog(@"QQ空间分享 - %d",[QQApiInterface SendReqToQZone:req]);
+        }
+    }
 }
 
 
