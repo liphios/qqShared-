d6fb507a078a726f1bf65cca8adc9f64463ba3cc
李鹏辉 <liphios@163.com>
2016-09-28 14:26:38 +0800

    Initial Commit

---
+++ 分享测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  分享测试
+//
+//  Created by 李鹏辉 on 16/9/28.
+//  Copyright © 2016年 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 分享测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  分享测试
+//
+//  Created by 李鹏辉 on 16/9/28.
+//  Copyright © 2016年 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
